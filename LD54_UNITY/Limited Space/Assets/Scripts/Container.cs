46a0d17fa014b208ca958789e11570558005d269
Kleissen,H.D.J. (Hilko) <h.d.j.kleissen@students.uu.nl>
2023-09-30 16:24:06 +0200

    Add basic asteroid hazard

--- LD54_UNITY/Limited Space/Assets/Scripts/Container.cs
+++ LD54_UNITY/Limited Space/Assets/Scripts/Container.cs
@@ -2,8 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Container : MonoBehaviour
+public class Container : MonoBehaviour, IDamageable
 {
+    public void Damage(float amount)
+    {
+        // Choose random amount of cargo items, then distribute damage. For now we log to console
+        Debug.Log(name + ": ow (" + amount + ")");
+    }
+
     // Start is called before the first frame update
     void Start()
     {
