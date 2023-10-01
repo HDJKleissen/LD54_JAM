f6f0b0e9ac2e186dfa8f1e9903f98f38cf0e08a3
Pepn <pepijn.thijssens@gmail.com>
2023-10-01 13:09:36 +0200

    add planets holding items

--- LD54_UNITY/Limited Space/Assets/Scripts/Planet.cs
+++ LD54_UNITY/Limited Space/Assets/Scripts/Planet.cs
@@ -6,6 +6,7 @@ public class Planet : MonoBehaviour
 {
     Transform player;
     public Transform ItemContainer;
+    public List<CarriageItem> items = new List<CarriageItem>();
     private void Awake()
     {
         player = FindObjectOfType<PlayerMovement>().transform;
@@ -14,12 +15,52 @@ public class Planet : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    // on trigger enter does not work in the case you drag immediately from wagon to planet; quick fix
+    // TODO: Improve performance lol
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.GetComponent<CarriageItem>())
+        {
+            CarriageItem item = collision.GetComponent<CarriageItem>();
+            if (item.carriageManager == null)
+            {
+                if (!items.Contains(item))
+                {
+                    items.Add(item);
+                }
+            }
+
+            if (item.carriageManager != null)
+            {
+                if (items.Contains(item))
+                {
+                    items.Remove(item);
+                }
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.GetComponent<CarriageItem>())
+        {
+            CarriageItem item = collision.GetComponent<CarriageItem>();
+            if (item.carriageManager == null)
+            {
+                if (items.Contains(item))
+                {
+                    items.Remove(item);
+                }
+            }
+        }
     }
 }
