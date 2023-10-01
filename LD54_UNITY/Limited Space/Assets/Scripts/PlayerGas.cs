3894b4372eff062ac66e7dfa273c8c550dcd8657
Pepn <pepijn.thijssens@gmail.com>
2023-10-01 17:39:43 +0200

    add gameover trigger on empty gas

--- LD54_UNITY/Limited Space/Assets/Scripts/PlayerGas.cs
+++ LD54_UNITY/Limited Space/Assets/Scripts/PlayerGas.cs
@@ -10,6 +10,8 @@ public class PlayerGas : MonoBehaviour
     [SerializeField] float maxGas = 100.0f;
     [SerializeField] float useGasMultiplier = 1.0f;
 
+    private bool isRanOut = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,6 +22,12 @@ public class PlayerGas : MonoBehaviour
     void Update()
     {
         gasSlider.value = gasAmount / maxGas;
+
+        if(gasAmount < 0 && !isRanOut)
+        {
+            isRanOut = true;
+            Debug.LogWarning("GAME OVER!!");
+        }
     }
 
     public void ReduceGas(float amount)
